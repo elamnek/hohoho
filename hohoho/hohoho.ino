d7c3066795076c98730483d57f5eb8b3d911a20f
Eugene Lamnek <eugene@geoit.com.au>
2022-12-22 08:28:29 +1030

    final testing

--- hohoho/hohoho.ino
+++ hohoho/hohoho.ino
@@ -35,7 +35,7 @@ SoftwareSerial  MP3Stream(ARDUINO_RX, ARDUINO_TX);  // MP3 player serial stream 
 //#define Console   Serial   // command processor input/output stream
 #endif
 
-auto timerHoHoHo = timer_create_default();
+auto timerMinute = timer_create_default();
 
 const uint8_t PLAY_FOLDER = 1;   // tracks are all placed in this folder
 MD_YX5300 mp3(MP3Stream);
@@ -43,10 +43,9 @@ MD_YX5300 mp3(MP3Stream);
 int m_intServoPin = 5;
 Servo m_servo;
 
-static int m_arraySoundID[13] = { 1,1,1,1,1,1,1,1,1,1,1,2,2};
-static long m_arrayData[13] = { 300,550,1000,1440,2250,2710,3060,3440,3900,4300,4800,60,1440};
-int intThisID = 1;
-
+long m_lngRandomMinutes;
+long m_lngRandomSound;
+long m_lngMinuteCounter = 0;
 
 void setup()
 {
@@ -54,41 +53,67 @@ void setup()
     m_servo.attach(m_intServoPin);
     m_servo.write(90);
 
-    timerHoHoHo.every(15000, timerHoHoHo_interrupt);
+    timerMinute.every(60000, timerMinute_interrupt);
 
     MP3Stream.begin(MD_YX5300::SERIAL_BPS);
     mp3.begin();
     mp3.setSynchronous(true);
     mp3.volume(30);
-    
+
+    m_lngRandomMinutes = random(1, 2);//always play sound in the first minute when starting up
+    m_lngRandomSound = random(4, 6);//two tracks to play (tracks 4 and 5)
   
- 
 }
 
-bool timerHoHoHo_interrupt(void*) {
+bool timerMinute_interrupt(void*) {
+   
+    //increment the minute counter
+    m_lngMinuteCounter = m_lngMinuteCounter + 1;
 
-    mp3.playTrack(4);
-    Serial.println("timer hit");
-    
-    delay(m_arrayBurp[0]);
-    for (int i = 0; i < 13; i++) {
+    Serial.println("timer hit - minute counter: " + String(m_lngMinuteCounter) + " random minutes: " + String(m_lngRandomMinutes) + " random sound: " + String(m_lngRandomSound));
+
+    if (m_lngMinuteCounter == m_lngRandomMinutes) {
+
+        //play the sound
+        mp3.playTrack(m_lngRandomSound);
+
+        //activate servo according to delay scenario
+        if (m_lngRandomSound == 4) {
+           
+            //hohoho
+            long arrayData[11] = { 300,550,1000,1440,2250,2710,3060,3440,3900,4300,4800 };
+            delay(arrayData[0]);
+            for (int i = 0; i < 11; i++) {
+                long lngOpenTime = (arrayData[i] - arrayData[i - 1]) / 2;
+                m_servo.write(60); //open 
+                delay(lngOpenTime);
+                m_servo.write(90); //close
+                delay(lngOpenTime);
+            }
+
+        } else if (m_lngRandomSound == 5){
+            
+            //burp
+            //long arrayData[2] = { 60,1440 };
+            delay(60);
+            m_servo.write(40); //open
+            delay(1380);
+            m_servo.write(90); //close
+        }
+
+        //reset random numbers
+        m_lngRandomMinutes = random(1, 2);
+        m_lngRandomSound = random(4, 6);
+        m_lngMinuteCounter = 0;
         
-        long lngOpenTime = (m_arrayBurp[i] - m_arrayBurp[i - 1]) / 2;
-        
-        m_servo.write(50); //open 
-        delay(lngOpenTime);
-        m_servo.write(90); //close
-        delay(lngOpenTime);
     }
-
-    intThisID = intThisID + 1;
-
+    
     return true;
 }
 
 void loop()
 {
-    timerHoHoHo.tick();
+    timerMinute.tick();
     mp3.check();    
 }
 
